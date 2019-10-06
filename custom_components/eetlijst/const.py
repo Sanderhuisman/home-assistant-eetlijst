02a99999c8a30b61abb9259becdb75f2e98fea25
Sander Huisman <r3dnax25195@gmail.com>
2019-10-06 14:46:31 +0200

    Added deadline and noticeboard sensors

---
+++ custom_components/eetlijst/const.py
@@ -0,0 +1,11 @@
+CONF_ATTRIBUTION = 'Data provided by Eetlijst'
+
+CONF_UTILISATION_DEADLINE = 'deadline'
+CONF_UTILISATION_NOTICEBOARD = 'noticeboard'
+
+CONF_UTILISATION_CONDITIONS = {
+    CONF_UTILISATION_DEADLINE:
+        ['Deadline', '', 'mdi:clock', 'timestamp'],
+    CONF_UTILISATION_NOTICEBOARD:
+        ['Noticeboard', '', 'mdi:note-outline', None],
+}
